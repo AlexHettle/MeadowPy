6ecfeb18920222f7c58edbf74f58b265c24f73d2
Alex Hettle <hettleal@msu.edu>
2026-08-04 13:56:52 -0500

    Version update

--- MeadowPy/meadowpy/constants.py
+++ MeadowPy/meadowpy/constants.py
@@ -2,7 +2,7 @@
 
 APP_NAME = "MeadowPy"
 APP_ID = "meadowpy.ide.meadowpy"
-VERSION = "1.2.0"
+VERSION = "1.3.0"
 CONFIG_DIR_NAME = ".meadowpy"
 SETTINGS_FILENAME = "settings.json"
 RECENT_FILES_MAX = 15
